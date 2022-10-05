c93a957102a7eb500c50360cf876f2a2979ebf93
Michell <Admin@TA-822-INS>
2022-10-05 15:15:44 +0200

    snap

---
+++ Kursus/LinqDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LinqDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mappe = new System.IO.DirectoryInfo(@"c:\Microsoft");
+            var filer = mappe.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+            Console.WriteLine($"Har fundet {filer.Length} i {mappe.Name}");
+
+            var f = filer.Where(i => i.Length > 100000).Skip(10).Take(5);
+            foreach (var item in f)
+            {
+                Console.WriteLine(item.FullName);
+            }
+        }
+    }
+
+    class MT
+    {
+        public long Length { get; set; }
+        public string Name { get; set; }
+    }
+}
